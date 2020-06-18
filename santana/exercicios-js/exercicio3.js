870ec5ca8fdce3bd2a1f4ce18ac9c29641429f51
clamuflage <ianssantana22@gmail.com>
2020-06-18 19:32:48 -0300

    exercicios js

---
+++ santana/exercicios-js/exercicio3.js
@@ -0,0 +1,5 @@
+function divi(a, b) {
+   return a / b;
+}
+
+console.log(divi(10, 2));
---
+++ santana/exercicios-js/exercicio4.js
@@ -0,0 +1,5 @@
+function graus(a) {
+    return a * 9/5 +32 
+}
+  const fahrenheit= graus(35)
+    console.log(fahrenheit);
