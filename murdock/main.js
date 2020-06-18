58c94fa5c68fcdf072231c1d717d7c248907958a
AnaMurdock <anamurdock15@gmail.com>
2020-06-18 19:39:47 -0300

    exercises

---
+++ murdock/main.js
@@ -0,0 +1,38 @@
+// Exercicio 1
+// Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
+// Considere sempre anos completos, e que um ano possui 365 dias.
+
+// function calcDaysOfLife(name, age){
+//     return (`${name}, você já viveu ${age * 365} dias!`)
+// }
+
+// console.log(calcDaysOfLife("Maria", 19));
+
+
+
+
+
+// Exercicio 3
+// Faça um algoritmo que receba dois números, calcule e mostre a divisão do primeiro número pelo segundo.
+// Sabe-se que o segundo número não pode ser zero, portanto não é necessário se preocupar com validações.
+
+// function math(numberOne, numberTwo){
+//     return numberOne / numberTwo
+// }
+
+// console.log(math(5, 2));
+
+
+
+
+
+// Exercicio 4
+// Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit.
+// Faça um algoritmo para ler uma temperatura Celsius e imprimi-Ia em Fahrenheit (pesquise como fazer este tipo de conversão).
+
+// function temperatureConverter(celsius){
+//     const result = (celsius * (9 / 5)) + 32;
+//     return result.toFixed(1)
+// }
+
+// console.log(temperatureConverter(37))
