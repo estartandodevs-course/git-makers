0186432d62aaeee53ecb959eb3d1b2b298b8edd6
Kauan Robert <kauanrobert359@gmail.com>
2020-06-19 06:54:49 -0300

    exercicios kauan

---
+++ robert/exercicios-js-aula04/exercicio4.js
@@ -0,0 +1,6 @@
+const stringFrutas = "banana-uva-manga" 
+const frutas = stringFrutas.split("-")
+
+console.log(frutas)
+
+// Declarei a varivavel, usei "split" e coloquei o separador que tava sendo usado
