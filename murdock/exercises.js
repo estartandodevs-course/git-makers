a19d3ea6060da3ef510adc31ab4600ac7639f8ae
Ana <anamurdock15@gmail.com>
2020-06-22 20:47:58 -0300

    exercícios - aula 01

--- murdock/exercises.js
+++ murdock/exercises.js
@@ -2,11 +2,11 @@
 // Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
 // Considere sempre anos completos, e que um ano possui 365 dias.
 
-// function calcDaysOfLife(name, age){
-//     return (`${name}, você já viveu ${age * 365} dias!`)
-// }
+function calcDaysOfLife(name, age){
+    return (`${name}, você já viveu ${age * 365} dias!`)
+}
 
-// console.log(calcDaysOfLife("Maria", 19));
+console.log(calcDaysOfLife("Maria", 19));
 
 
 
@@ -17,22 +17,21 @@
 // Escreva um algoritmo que leia o peso do prato montado pelo cliente (em quilos) e imprima o valor a pagar.
 
 
-// const weight = 0.5;
-// const oneHundredGrams = 1.2;
-// const result = (weight * oneHundredGrams) * 10;
+const weight = 0.5;
+const result = weight * 12;
 
-// console.log(`O valor do seu prato é R$${result}`);
+console.log(`O valor do seu prato é R$${result}`);
 
 
 // Exercicio 3
 // Faça um algoritmo que receba dois números, calcule e mostre a divisão do primeiro número pelo segundo.
 // Sabe-se que o segundo número não pode ser zero, portanto não é necessário se preocupar com validações.
 
-// function math(numberOne, numberTwo){
-//     return numberOne / numberTwo
-// }
+function math(numberOne, numberTwo){
+    return numberOne / numberTwo
+}
 
-// console.log(math(5, 2));
+console.log(math(5, 2));
 
 
 
@@ -42,9 +41,9 @@
 // Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit.
 // Faça um algoritmo para ler uma temperatura Celsius e imprimi-Ia em Fahrenheit (pesquise como fazer este tipo de conversão).
 
-// function temperatureConverter(celsius){
-//     const result = (celsius * (9 / 5)) + 32;
-//     return result.toFixed(1)
-// }
+function temperatureConverter(celsius){
+    const result = (celsius * (9 / 5)) + 32;
+    return result.toFixed(1)
+}
 
-// console.log(temperatureConverter(37))
+console.log(temperatureConverter(37))
